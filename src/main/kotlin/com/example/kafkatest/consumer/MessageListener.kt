b70b9a0fdfc24f21ab9d07f7d8193ecb78d6a1a7
junggernaut <seungan1419@gmail.com>
2022-03-06 06:07:52 +0900

    connect err

--- src/main/kotlin/com/example/kafkatest/consumer/MessageListener.kt
+++ src/main/kotlin/com/example/kafkatest/consumer/MessageListener.kt
@@ -2,21 +2,22 @@ package com.example.kafkatest.consumer
 
 import com.example.kafkatest.constans.KafkaConstants
 import com.example.kafkatest.model.Message
-import groovy.util.logging.Slf4j
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Component
 
 
-@Slf4j
 @Component
 class MessageListener(
     @Autowired
-    private var template: SimpMessagingTemplate
+    private var template: SimpMessagingTemplate,
 ) {
+
     @KafkaListener(topics = [KafkaConstants.KAFKA_TOPIC], groupId = KafkaConstants.GROUP_ID)
-    fun listen(message: Message?) {
+    fun listen(message: Message) {
         template.convertAndSend("/topic/group", message)
     }
 }
--- src/main/kotlin/com/example/kafkatest/controller/ChatController.kt
+++ src/main/kotlin/com/example/kafkatest/controller/ChatController.kt
@@ -1,4 +1,36 @@
 package com.example.kafkatest.controller
 
+import com.example.kafkatest.constans.KafkaConstants
+import com.example.kafkatest.model.Message
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
+
+
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = ["/kafka"])
 class ChatController {
+    @Autowired
+    private lateinit var kafkaTemplate: KafkaTemplate<String, Message>
+    @PostMapping(value = ["/publish"])
+    fun sendMessage(@RequestBody message: Message) {
+        message.timestamp = LocalDateTime.now().toString()
+        try {
+            kafkaTemplate.send(KafkaConstants.KAFKA_TOPIC, message).get()
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/group")
+    fun broadcastGroupMessage(@Payload message: Message): Message {
+        return message
+    }
 }
